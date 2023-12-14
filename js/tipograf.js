245db3a4dc33976dcd3c774e1cfe0a60e0fdd11a
wayz-x <wayz.x.team@gmail.com>
2023-12-14 18:43:34 +0600

    tune and type

---
+++ js/tipograf.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('p, span, h1, h2, h3, h4, h5, small, strong').each(function() {
+        st = $(this).html();
+        st = st.replaceAll('  ', ' ');
+        st = st.replace(/(?!\s)--(?=\s)/g, '&mdash;')                   //длинное тире
+        st = st.replace(/(?<![\s\d])-(?![\d\s])/g, '&ndash;')           //короткое тире
+        st = st.replace(/-(?=\d)/g, '&minus;')                          //минус
+        st = st.replace(/(^|[\s;>\(\[-])["']/g, '$1&laquo;')                //ёлочки слева
+        st = st.replace(/["'](?=[\s-\.!,:;\?\)\]\n\r<]|$)/g, '&raquo;')     //ёлочки справа
+        st = st.replace(/(?<=(^|\s)[\p{L}]{1,3}|\d|%) /gu, '&nbsp;')    //висячие предлоги
+        st = st.replaceAll('&nbsp; ', '&nbsp;');
+        $(this).html(st);
+    });
+})
